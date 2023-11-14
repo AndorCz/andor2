56527fe6469e5829d3d6f91766b4b038cf900255
David Spohr <spohr.david@gmail.com>
2023-11-14 02:21:14 +0100

    Added auth basics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,25 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import { loadEnv } from 'vite'
+import auth from 'auth-astro'
+import vercel from '@astrojs/vercel/serverless'
+import Google from '@auth/core/providers/google'
+
+const env = loadEnv('', process.cwd(), '')
 
 // https://astro.build/config
-export default defineConfig({});
+
+export default defineConfig({
+  integrations: [
+    auth({
+      secret: env.AUTH_SECRET,
+      providers: [
+        Google({
+          clientId: env.GOOGLE_ID,
+          clientSecret: env.GOOGLE_SECRET
+        })
+      ]
+    })
+  ],
+  output: 'server',
+  adapter: vercel()
+})
---
+++ auth.config.js
@@ -0,0 +1,14 @@
+import { loadEnv } from 'vite'
+import Google from '@auth/core/providers/google'
+
+const env = loadEnv('all', process.cwd(), '')
+ 
+export default {
+  secret: env.AUTH_SECRET,
+  providers: [
+    Google({
+      clientId: env.GOOGLE_ID,
+      clientSecret: env.GOOGLE_SECRET
+    })
+  ]
+}
