bb94c6f80020974d80e8ce902938acdb4d1cb4ce
David Spohr <spohr.david@gmail.com>
2023-12-01 00:29:31 +0100

    Fixed logout

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,14 +1,21 @@
 <script>
   export let user
+
+  function logout () {
+    // delete cookies
+    document.cookie = 'sb-access-token=; Max-Age=-99999999;'
+    document.cookie = 'sb-refresh-token=; Max-Age=-99999999;'
+    window.location.href = './api/auth/logout'
+  }
 </script>
 
 <aside>
   {#if user.name}
     {user.name}<br><br>
-    <a href='./api/auth/logout'>Odhlásit</a>
+    <button on:click={logout}>Odhlásit</button>
   {:else if user.email}
     {user.email}<br><br>
-    <a href='./api/auth/logout'>Odhlásit</a>
+    <button on:click={logout}>Odhlásit</button>
   {:else}
     <form action='/api/auth/login' method='post'>
       <button value='google' name='provider' type='submit' class='google w100'>Přihlásit přes Google</button>
@@ -24,10 +31,6 @@
     background-color: var(--panel);
     border-radius: 0px 0px 10px 10px;
   }
-    aside a {
-      font-family: 'Alegreya Sans';
-    }
-
   .w100 {
     width: 100%;
   }
