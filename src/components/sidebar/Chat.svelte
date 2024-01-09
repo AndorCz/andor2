44cf6d7d10fbfa39deeb61cda72e7f71b63c1932
David Spohr <spohr.david@gmail.com>
2024-01-09 02:52:47 +0100

    Added realtime chat basics

--- src/components/sidebar/Chat.svelte
+++ src/components/sidebar/Chat.svelte
@@ -1,18 +1,48 @@
 <script>
   import { supabase, handleError } from '@lib/database'
+  import { beforeUpdate, afterUpdate, onDestroy } from 'svelte'
+  import { writable } from 'svelte/store'
 
   export let user
   export let userStore
 
   let contact = {}
-  let messages = []
+  let loadingMessages = true
+  let messagesEl
+  let inputEl
+  let channel
+
+  const messages = writable([])
+  const contactId = $userStore.openChat
+  const sortedIds = [user.id, contactId].sort()
+
+  beforeUpdate(() => {
+    // init conversation, listen for new messages in the conversation
+    channel = supabase
+      .channel(`private-chat-${sortedIds[0]}-${sortedIds[1]}`)
+      .on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'messages', filter: `((recipient=eq.${user.id} and sender=eq.${contactId}) or (sender=eq.${user.id} and recipient=eq.${contactId}))` }, (payload) => {
+        $messages.push(payload.new)
+        $messages = $messages // update store
+      })
+      .subscribe()
+    loadMessages().catch(console.error)
+  })
+
+  afterUpdate(() => { // scroll down
+    if (messages.length) { messagesEl.lastElementChild.scrollIntoView({ behavior: 'smooth' }) }
+  })
+
+  onDestroy(() => {
+    console.log('destroy fired', channel)
+    if (channel) { supabase.removeChannel(channel) }
+  })
 
   async function waitForAnimation () {
     return new Promise(resolve => setTimeout(resolve, 200))
   }
 
   async function loadContact () {
-    const { data, error } = await supabase.from('profiles').select('*').eq('id', $userStore.openChat).single()
+    const { data, error } = await supabase.from('profiles').select('*').eq('id', contactId).single()
     if (error) { return handleError(error) }
     contact = data
   }
@@ -20,22 +50,36 @@
   async function loadMessages () {
     // load messages for both uid and user.id, in either sender or recipient column
     const { data, error } = await supabase.from('messages').select('*')
-      .or(`sender.eq.${$userStore.openChat},recipient.eq.${$userStore.openChat}`)
+      .or(`sender.eq.${contactId},recipient.eq.${contactId}`)
       .or(`sender.eq.${user.id},recipient.eq.${user.id}`)
       .order('created_at', { ascending: true })
     if (error) { return handleError(error) }
-    messages = data
+    $messages = data
+    loadingMessages = false
   }
 
   function closeChat () {
     $userStore.openChat = null
+  }
+
+  async function sendMessage () {
+    const { error } = await supabase.from('messages').insert({ sender: user.id, recipient: contact.id, content: inputEl.value })
+    if (error) { return handleError(error) }
+    inputEl.value = ''
+  }
+
+  function onKeyDown (e) {
+    if (event.keyCode === 13 && !e.shiftKey) { // send with enter, new line with shift+enter
+      sendMessage()
+      e.preventDefault()
+    }
   }
 </script>
 
 {#await waitForAnimation() then}
   <div id='chat'>
     <button on:click={closeChat} id='close' title='zavřít' class='material'>close</button>
-    {#if $userStore.openChat && user?.id}
+    {#if contactId && user?.id}
       {#await Promise.all([loadContact(), loadMessages()])}
         <span class='loading'>Načítám konverzaci...</span>
       {:then value}
@@ -46,18 +90,31 @@
             <div class='subtitle'>soukromá konverzace</div>
           </div>
         </h2>
-        <div class='messages'>
-          {#if messages.length}
-            {#each messages as message}
-              <div class='message'>
-                <span class='text'>{message.content}</span>
-                <span class='time'>{message.created_at}</span>
+
+        <div class='messages' bind:this={messagesEl}>
+          {#if loadingMessages}
+            <center class='loading'>Načítání...</center>
+          {:else if $messages.length > 0}
+            {#each $messages as message}
+              <div class='messageRow'>
+                <!-- add tippy for time -->
+                <div class='message {message.sender === user.id ? 'mine' : 'theirs'}' title={(new Date(message.created_at)).toLocaleString('cs')}>
+                  <!-- add 'read' column -->
+                  {#if !message.read}
+                    <div class='badge'></div>
+                  {/if}
+                  {message.content}
+                </div>
               </div>
+              <div class='clear'></div>
             {/each}
           {:else}
-            <span class='empty'>Žádné zprávy</span>
+            <center>Žádné zprávy</center>
           {/if}
         </div>
+        <!-- svelte-ignore a11y-autofocus -->
+        <textarea type='text' bind:this={inputEl} on:keydown={onKeyDown} maxlength='4096' autofocus></textarea>
+        <button class='material send' title='Odeslat' on:click={() => sendMessage()}>send</button>
       {:catch error}
         <span class='error'>Konverzaci se nepodařilo načíst</span>
       {/await}
@@ -114,5 +171,59 @@
     }
     .error {
       color: var(--error);
+    }
+
+    .messages {
+      flex: 1;
+      overflow-y: scroll;
+      padding: 5px 20px 10px 20px;
+    }
+      .clear {
+        clear: both;
+        overflow: auto;
+      }
+      .messageRow {
+        margin: 5px 0px;
+      }
+        .message {
+          position: relative;
+          max-width: 90%;
+          padding: 10px 20px;
+        }
+          .badge {
+            position: absolute;
+            top: -5px;
+            right: -5px;
+            width: 15px;
+            height: 15px;
+            border-radius: 50%;
+            box-shadow: 1px 1px 4px #0005;
+            background-color: var(--accent);
+          }
+          .theirs {
+            border-radius: 20px 20px 20px 0px;
+            background-color: var(--background);
+            text-align: left;
+            float: left;
+          }
+          .mine {
+            border-radius: 20px 20px 0px 20px;
+            background-color: var(--accent);
+            color: var(--gray90);
+            text-align: right;
+            float: right;
+          }
+    textarea {
+      flex: 0 0 48px;
+      display: block;
+      width: 85%;
+      background-color: var(--gray90);
+      overflow: hidden;
+      resize: none;
+    }
+    .send {
+      position: absolute;
+      bottom: 6px;
+      right: 5px;
     }
 </style>
