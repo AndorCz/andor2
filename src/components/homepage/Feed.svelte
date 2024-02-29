8791cb9201591b8fc7c940eb76365b197dfab1a0
David Spohr <spohr.david@gmail.com>
2024-02-29 22:51:41 +0100

    Fix for feed

--- src/components/homepage/Feed.svelte
+++ src/components/homepage/Feed.svelte
@@ -4,32 +4,33 @@
   export let lastPosts = []
   export let user = {}
 
-  const grouped = lastPosts?.reduce((acc, item) => {
-    let group = acc.find(g => g.content_id === item.content_id)
-    if (!group) {
-      group = { contentId: item.content_id, contentType: item.content_type, contentName: item.content_name, latest: item.created_at, posts: [] }
-      acc.push(group)
-    }
-    group.posts.push(item)
-    if (new Date(item.created_at) > new Date(group.latest)) {
-      group.latest = item.created_at
-    }
-    return acc
-  }, [])
+  const groups = []
 
-  const sortedGroups = grouped?.sort((a, b) => new Date(b.latest) - new Date(a.latest)).map(({ contentId, contentType, contentName, posts }) => ({ contentId, contentType, contentName, posts }))
+  lastPosts.forEach(post => {
+    const group = groups.find(group => group.headline.contentId === post.content_id)
+    if (group) {
+      group.posts.push(post)
+    } else {
+      groups.push({
+        headline: {
+          contentId: post.content_id,
+          contentName: post.content_name,
+          contentType: post.content_type
+        },
+        posts: [post]
+      })
+    }
+  })
 </script>
 
 <h3>Poslední příspěvky</h3>
 <div id='lastPosts'>
-  {#if sortedGroups}
-    {#each sortedGroups as group}
-      {#if group.posts.length > 0}
-        <a href={`/${group.contentType}/${group.contentId}`}><h4>{group.contentName}</h4></a>
-        {#each group.posts as post}
-          <Post {post} {user} iconSize={80} />
-        {/each}
-      {/if}
+  {#if groups}
+    {#each groups as group}
+      <a href={`/${group.headline.contentType}/${group.headline.contentId}`}><h4>{group.headline.contentName}</h4></a>
+      {#each group.posts as post}
+        <Post {post} {user} iconSize={80} />
+      {/each}
     {/each}
   {/if}
 </div>
