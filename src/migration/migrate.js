e33a4610b41661048e261ed8976ad5f5cdc7debb
David Spohr <spohr.david@gmail.com>
2024-03-30 00:06:40 +0100

    Migrate dependency, polish

--- src/migration/migrate.js
+++ src/migration/migrate.js
@@ -1,46 +1,29 @@
-import dotenv from 'dotenv';
-import { createHash } from 'crypto';
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
 
-dotenv.config();
+import { createHash } from 'node:crypto'
+import { open } from 'sqlite'
+import sqlite3 from 'sqlite3'
 
+const dbPath = import.meta.env.SQLITE_PATH
 
-const dbPath = process.env.SQLITE_PATH
-
-async function queryDatabase(sqlQuery) {
-  try {
-    // Open the database
-    const db = await open({
-      filename: dbPath,
-      driver: sqlite3.Database
-    });
-
-    // get data
-    const row = await db.get(sqlQuery);
-    
-    // Close the database connection
-    await db.close();
-    return row
-
-    } catch (err) {
-        throw err;
-    }
+async function queryDatabase (sqlQuery) {
+  const db = await open({ filename: dbPath, driver: sqlite3.Database })
+  const row = await db.get(sqlQuery)
+  await db.close()
+  return row
 }
 
-
-export async function getOldUserInfo(login, password) {
-    const hashedPassword = createHash('md5').update(password).digest('hex');
-    const sqlQuery = `SELECT old_id, old_login, old_email FROM old_users WHERE old_login = '${login}' AND old_psw = '${hashedPassword}'`;
-    const results = await queryDatabase(sqlQuery);
-    return results
-}
-
-export async function getOldUserLogin(id){
-  const sqlQuery = `SELECT old_login FROM old_users WHERE old_id = '${id}'`;
-  const results = await queryDatabase(sqlQuery);
+export async function getOldUserInfo (login, password) {
+  const hashedPassword = createHash('md5').update(password).digest('hex')
+  const sqlQuery = `SELECT old_id, old_login, old_email FROM old_users WHERE old_login = '${login}' AND old_psw = '${hashedPassword}'`
+  const results = await queryDatabase(sqlQuery)
   return results
 }
 
-// Testing lol
+export async function getOldUserLogin (id) {
+  const sqlQuery = `SELECT old_login FROM old_users WHERE old_id = '${id}'`
+  const results = await queryDatabase(sqlQuery)
+  return results
+}
+
+// Testing
 // getOldUserInfo('Avium','123')
