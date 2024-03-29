688cda8666b1499f22e91b24af1cbef09f9b8b0e
David Spohr <spohr.david@gmail.com>
2024-03-29 10:58:34 +0100

    Updated error handling of callback

--- src/pages/api/auth/callback.js
+++ src/pages/api/auth/callback.js
@@ -3,16 +3,19 @@ import { getSupabase } from '@lib/database'
 import { saveAuthCookies } from '@lib/utils'
 
 export const GET = async ({ request, cookies, redirect }) => {
-  const requestUrl = new URL(request.url)
-  const code = requestUrl.searchParams.get('code')
-  const next = requestUrl.searchParams.get('next') || '/'
-
-  if (code) {
-    const supabase = getSupabase(cookies)
-    const { data, error } = await supabase.auth.exchangeCodeForSession(code)
-    saveAuthCookies(cookies, data.session)
-    if (!error) { return redirect(next) }
+  try {
+    const requestUrl = new URL(request.url)
+    const code = requestUrl.searchParams.get('code')
+    const next = requestUrl.searchParams.get('next') || '/'
+    if (code) {
+      const supabase = getSupabase(cookies)
+      const { data, error } = await supabase.auth.exchangeCodeForSession(code)
+      saveAuthCookies(cookies, data.session)
+      if (error) { return redirect(`/?toastType=error&toastText=${encodeURIComponent('Chyba: ' + error.message)}`) }
+      return redirect(next)
+    }
+  } catch (unhandledError) {
+    return redirect(`/?toastType=error&toastText=${encodeURIComponent('Chyba: ' + unhandledError.message)}`)
   }
-
   return new Response('Přihlášení se nezdařilo. Zkus to prosím znovu.', { status: 500 })
 }
