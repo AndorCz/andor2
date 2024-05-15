53a7353cd43d9f2cd66c6dfb98dd3adb91369e51
David Spohr <spohr.david@gmail.com>
2024-05-16 01:12:55 +0200

    News polish

--- src/components/homepage/NewsSection.svelte
+++ src/components/homepage/NewsSection.svelte
@@ -23,7 +23,7 @@
     {#each items as item}
       <div class='item'>
         {#if item.custom_header}
-          <img src={getHeaderUrl(slug, item.id, item.custom_header)} alt={item.name} class='header' />
+          <img src={getHeaderUrl(slug, item.id, item.custom_header)} alt={item.name} class='image' />
         {/if}
         <main>
           <a href={`/${slug}/${item.id}`} class='name'><h2>{item.name}</h2></a>
@@ -44,59 +44,67 @@
 {/await}
 
 <style>
+  h3 {
+    margin-top: 0px;
+    font-size: 20px;
+  }
   #news {
     display: flex;
     flex-direction: column;
     gap: 10px;
   }
-    .item .header {
-      max-width: 100%;
-      display: block;
-      min-height: 100px;
-      object-fit: cover;
+    .item {
+      display: flex;
+      width: 100%;
     }
-    main {
-      background-color: var(--block);
-      padding: 20px;
-      padding-bottom: 10px;
-    }
-      .name {
+      .item .image {
+        max-width: 100px;
         display: block;
+        object-fit: cover;
       }
-        .name h2 {
-          margin-top: 0px;
-          margin-bottom: 10px;
-          font-size: 26px;
-          line-height: 120%;
-        }
-      .annotation {
-        font-size: 18px;
-        font-style: italic;
-        font-variation-settings: 'wght' 400;
-        margin: 5px 0px;
+      main {
+        flex: 1;
+        background-color: var(--block);
+        padding: 20px;
+        padding-bottom: 10px;
       }
-        .details {
-          display: flex;
-          justify-content: space-between;
+        .name {
+          display: block;
         }
-        .date {
-          display: flex;
-          align-items: center;
-          font-size: 16px;
-          color: var(--dim);
-        }
-        .user {
-          display: flex;
-          align-items: center;
-          gap: 10px;
-        }
-          .portrait {
-            display: block;
-            width: 40px;
-            height: 40px;
-            object-fit: cover;
-            object-position: center 20%;
-            border-radius: 100%;
-            background-color: var(--background);
+          .name h2 {
+            margin-top: 0px;
+            margin-bottom: 10px;
+            font-size: 22px;
+            line-height: 120%;
           }
+        .annotation {
+          font-size: 18px;
+          font-style: italic;
+          font-variation-settings: 'wght' 400;
+          margin: 5px 0px;
+        }
+          .details {
+            display: flex;
+            justify-content: space-between;
+          }
+          .date {
+            display: flex;
+            align-items: center;
+            font-size: 16px;
+            color: var(--dim);
+          }
+          .user {
+            display: flex;
+            align-items: center;
+            gap: 10px;
+          }
+            .portrait {
+              display: block;
+              width: 40px;
+              height: 40px;
+              object-fit: cover;
+              object-position: center 20%;
+              border-radius: 100%;
+              background-color: var(--background);
+            }
 </style>
