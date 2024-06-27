e496938ea6b0be31514336adfbd531dfbedf8984
AlexandraBot <alexan.gary@gmail.com>
2024-06-27 15:41:59 +0200

    preparing for skins
    Added user default settings,
    Added skins folder tree,
    Added example for skin (sidebar>user)

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,10 @@ import { getSupabase, saveAuthCookies } from '@lib/database-server'
 
 export async function onRequest ({ cookies, locals, redirect, url, context }, next) {
   try {
-    locals.user = {} // default empty user object
+    // default empty user object
+    locals.user = {
+      'skin' : 'obsidian'
+    } 
 
     const accessToken = cookies.get('sb-access-token')?.value
     const refreshToken = cookies.get('sb-refresh-token')?.value
@@ -24,14 +27,14 @@ export async function onRequest ({ cookies, locals, redirect, url, context }, ne
 
       if (authData.user) {
         saveAuthCookies(cookies, authData.session)
-        locals.user = { id: authData.user.id, email: authData.user.email }
+        locals.user = Object.assign(locals.user, { id: authData.user.id, email: authData.user.email })
       }
 
       // Load user-specific data if user is authenticated
       if (locals.user?.id) {
         const { data: profileData } = await supabase.from('profiles').select('*').eq('id', locals.user.id).maybeSingle()
         if (profileData?.name) {
-          locals.user = { ...profileData, ...locals.user }
+          locals.user = Object.assign(locals.user, profileData)
           const { error: profileError } = await locals.supabase.from('profiles').update({ last_activity: new Date() }).eq('id', locals.user.id)
           if (profileError) { throw profileError }
         } else if (url.pathname !== '/onboarding') {
