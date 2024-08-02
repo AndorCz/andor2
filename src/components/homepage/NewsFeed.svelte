89581768bc347fb2466dbee232221992c81f1884
David Spohr <spohr.david@gmail.com>
2024-08-02 18:43:16 +0200

    Language fix

--- src/components/homepage/NewsFeed.svelte
+++ src/components/homepage/NewsFeed.svelte
@@ -13,7 +13,7 @@
 </script>
 
 <div id='news'>
-  <h3>Upoutávky<a href='https://andor2.cz/board/35' class='material' title='Chceš propagovat svojí hru, diskuzi, či dílo? Popiš svojí představu do diskuze "Zadání upotávky", kam tě vezme kliknutí na tuto ikonku.' use:tooltip>info</a></h3>
+  <h3>Upoutávky<a href='https://andor2.cz/board/35' class='material' title='Chceš propagovat hru, diskuzi, či dílo? Popiš svoji představu do diskuze "Zadání upoutávky", kam tě vezme kliknutí na tuto ikonku.' use:tooltip>info</a></h3>
   {#each news as item}
     <NewsItem {user} {item} />
   {/each}
