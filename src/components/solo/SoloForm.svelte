b048ba60681f66e1b6263df75cada0b4fa548feb
David Spohr <spohr.david@gmail.com>
2025-06-15 16:57:30 +0200

    Concept creation wip

--- src/components/solo/SoloForm.svelte
+++ src/components/solo/SoloForm.svelte
@@ -4,8 +4,10 @@
   import { gameTags } from '@lib/constants'
 
   export let user = {}
+
   let selectedTags
   let tagsInputRef
+  let showAdvanced = false
 
   const prepareData = async (event) => {
     event.preventDefault()
@@ -28,17 +30,47 @@
     </div>
 
     <div class='row'>
-      <div class='labels'><label for='conceptAnnotation'>Anotace</label></div>
-      <div class='inputs'><TextareaExpandable placeholder='Popiš o čem by hra měla být, v jakém světě se odehrává, jakou by měla mít atmosféru apod. Na základě tohoto popisu AI vygeneruje konkrétní příběhové linie.' {user} id='conceptAnnotation' name='conceptAnnotation' minHeight={150} maxlength={350} /></div>
+      <div class='labels'><label for='conceptDescription'>Prostředí</label></div>
+      <div class='inputs'><TextareaExpandable placeholder='V jakém světě a časovém období se hra odehrává?' {user} id='worldDescription' name='worldDescription' minHeight={75} maxlength={1000} /></div>
     </div>
 
-    <!-- 2DO: require image, offer generating -->
     <div class='row'>
-      <div class='labels'><label for='soloImage'>Obrázek</label></div>
-      <div class='inputs'>
-        <input type='file' id='soloImage' name='soloImage' accept='image/*' />
-      </div>
+      <div class='labels'><label for='conceptStory'>Příběh</label></div>
+      <div class='inputs'><TextareaExpandable placeholder='O čem hra bude? Stačí hlavní zápletka nebo motiv.' {user} id='conceptStory' name='conceptStory' minHeight={75} maxlength={1000} /></div>
     </div>
+
+    <div class='row'>
+      <div class='labels'><label for='conceptProtagonist'>Protagonista</label></div>
+      <div class='inputs'><TextareaExpandable placeholder='Koho hráč hraje? Je něčím omezen výběr postavy?' {user} id='conceptProtagonist' name='conceptProtagonist' minHeight={75} maxlength={1000} /></div>
+    </div>
+
+    {#if showAdvanced}
+      <center><button type='button' class='small' on:click={() => { showAdvanced = false }}>Skrýt pokročilé</button></center>
+
+      <div class='row'>
+        <div class='labels'><label for='conceptLocations'>Místa</label></div>
+        <div class='inputs'><TextareaExpandable placeholder='Jaká místa jsou pro hru důležitá?' {user} id='conceptLocations' name='conceptLocations' minHeight={75} maxlength={1000} /></div>
+      </div>
+
+      <div class='row'>
+        <div class='labels'><label for='conceptFactions'>Frakce</label></div>
+        <div class='inputs'><TextareaExpandable placeholder='Jaké frakce, organizace nebo skupiny jsou ve hře důležité?' {user} id='conceptFactions' name='conceptFactions' minHeight={75} maxlength={1000} /></div>
+      </div>
+
+      <div class='row'>
+        <div class='labels'><label for='conceptCharacters'>Postavy</label></div>
+        <div class='inputs'><TextareaExpandable placeholder='Jaké postavy jsou pro hru důležité?' {user} id='conceptCharacters' name='conceptCharacters' minHeight={75} maxlength={1000} /></div>
+      </div>
+
+      <div class='row'>
+        <div class='labels'><label for='soloImage'>Ilustrace</label></div>
+        <div class='inputs'>
+          <TextareaExpandable placeholder='Popiš vizuálně obrázek který by hru nejlépe vystihoval' {user} id='soloImage' name='soloImage' minHeight={75} maxlength={350} />
+        </div>
+      </div>
+    {:else}
+      <center><button type='button' class='small' on:click={() => { showAdvanced = true }}>Zobrazit pokročilé</button></center>
+    {/if}
 
     <div class='row'>
       <div class='labels'><label for='workTags'>Tagy<span class='info'>(max 3)</span></label></div>
