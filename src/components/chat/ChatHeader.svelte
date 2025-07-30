6ddaf8adc4073d90010f5e86926f7bbf53c66358
David Spohr <spohr.david@gmail.com>
2025-07-30 21:44:02 +0200

    Chat toast fix

---
+++ src/components/chat/ChatHeader.svelte
@@ -0,0 +1,44 @@
+<script>
+  import { onMount } from 'svelte'
+  import { initToasts } from '@lib/toasts'
+
+  onMount(() => {
+    initToasts()
+  })
+</script>
+
+<ul id='header'>
+  <li><a href='/'><span class='material'>star</span><span>Novinky</span></a></li>
+  <li><a href='/games'><span class='material'>casino</span><span>Hry</span></a></li>
+  <li><a href='/works'><span class='material'>edit</span><span>Tvorba</span></a></li>
+  <li><a href='/boards'><span class='material'>forum</span><span>Diskuze</span></a></li>
+  <li><a href='/chat' class='active'><span class='material'>chat</span><span>Chat</span></a></li>
+</ul>
+
+<style>
+  #header {
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    padding: 15px;
+    margin: 0px;
+    z-index: 999;
+    background-color: var(--panel);
+    box-shadow: 2px 2px 4px 0px #00000040;
+    display: flex;
+    gap: 20px;
+  }
+    #header li {
+      list-style-type: none;
+    }
+      #header li a {
+        display: flex;
+        gap: 10px;
+        align-items: center;
+        text-decoration: none;
+      }
+      #header li a.active {
+        color: var(--text);
+      }
+</style>
