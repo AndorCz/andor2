2d5e1b5e81bc87dbd6554502afb06b08be40938d
David Spohr <spohr.david@gmail.com>
2025-09-08 02:20:40 +0200

    Finished media page

--- src/components/Media.svelte
+++ src/components/Media.svelte
@@ -11,6 +11,7 @@
 <h1>Propagační materiály</h1>
 <h2>Texty</h2>
 <p>"Andor2.cz je online platforma pro hraní stolních RPG formou textových příspěvků. Staví na komunitě CZ/SK hráčů, nabízí diskuzní fóra a další uživatelskou tvorbu. Nově přidává sólo hry s AI vypravěčem."</p>
+
 <h2>Logo</h2>
 <h3>Vektor</h3>
 <div class='grid'>
@@ -42,25 +43,68 @@
     <p class='label'>Mramor 1024px</p>
   </div>
 </div>
+
+<h2>Bannery</h2>
+<div class='grid'>
+  <div class='card'>
+    <img src='/media/banner/banner1.jpg' alt='Andor 2 banner 1' />
+    <p class='label'>Banner 1</p>
+  </div>
+</div>
+
 <h2>Obrázky</h2>
 <div class='card'>
-  <img src='/media/images/header_spring.jpg' alt='Andor 2 obrázek jaro' />
-  <p class='label'>Hlavička jaro</p>
-</div>
-<div class='card'>
-  <img src='/media/images/header_winter.jpg' alt='Andor 2 obrázek zima' />
-  <p class='label'>Hlavička zima</p>
-</div>
-<div class='card'>
-  <img src='/media/images/spring.jpg' alt='Andor 2 upscale jaro' />
+  <img src='/media/art/spring.jpg' alt='Andor 2 upscale jaro' />
   <p class='label'>Upscale jaro</p>
 </div>
 <div class='card'>
-  <img src='/media/images/winter.jpg' alt='Andor 2 upscale zima' />
+  <img src='/media/art/winter.jpg' alt='Andor 2 upscale zima' />
   <p class='label'>Upscale zima</p>
 </div>
+<div class='card'>
+  <img src='/media/art/art1.jpg' alt='Andor 2 art 1' />
+  <p class='label'>Art 1</p>
+</div>
+<div class='card'>
+  <img src='/media/art/art2.jpg' alt='Andor 2 art 2' />
+  <p class='label'>Art 2</p>
+</div>
+<div class='card'>
+  <img src='/media/art/art3.jpg' alt='Andor 2 art 3' />
+  <p class='label'>Art 3</p>
+</div>
+
+<h2>Barvy</h2>
+<div class='grid'>
+  <div class='card' style='background: #FFD700; color: black'>Zlatá #FFD700</div>
+  <div class='card' style='background: #c48d66; color: white'>Oranžová #c48d66</div>
+  <div class='card' style='background: #634b41; color: white'>Hnědá 1 #634b41</div>
+  <div class='card' style='background: #3c3936; color: white'>Hnědá 2 #3c3936</div>
+  <div class='card' style='background: #a5a664; color: black'>Zelená #a5a664</div>
+</div>
+
 <h2>QR kód</h2>
+<div class='grid'>
+  <div class='card' onclick={() => { download('/media/qr/qr.png', 'qr.png') }}>
+    <img src='/media/qr/qr.png' alt='Andor 2 QR PNG' />
+    <p class='label'>Image</p>
+  </div>
+  <div class='card' onclick={() => { download('/media/qr/qr_black.png', 'qr_black.png') }}>
+    <img src='/media/qr/qr_black.png' alt='Andor 2 QR Black' />
+    <p class='label'>Black</p>
+  </div>
+  <div class='card' onclick={() => { download('/media/qr/qr_white.png', 'qr_white.png') }}>
+    <img src='/media/qr/qr_white.png' alt='Andor 2 QR White' />
+    <p class='label'>White</p>
+  </div>
+  <div class='card' onclick={() => { download('/media/qr/qr.svg', 'qr.svg') }}>
+    <img src='/media/qr/qr_black.png' alt='Andor 2 QR SVG' />
+    <p class='label'>SVG</p>
+  </div>
+</div>
+
 <h2>Balíček</h2>
+<p>Všechny výše uvedené materiály jsou dostupné v jednom ZIP balíčku: <a href='https://www.dropbox.com/scl/fi/a5o34bghpta69ihrsecb0/media.zip?rlkey=29ioolm76bp5hf45lp52wbiy0&dl=1' download>media.zip</a> (6.2 MB)</p>
 
 <style>
   .grid {
